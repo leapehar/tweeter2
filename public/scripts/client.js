5261005e453227c1d48c9625693bf06f36b29042
Lea Pehar <leapehar@gmail.com>
2022-02-03 07:40:45 +0000

    implemented renderTweets function, which takes in an array of tweet objects and appends each one to the #tweets-container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,61 +12,84 @@
 
 $(document).ready(function() {
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1643618373314
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1643704773314
+    }
+  ]
+
+  // renderTweets function
+  // takes in an arary of tweets
+  // appends each item in the array to #tweets-container
+  // leverages createTweetElement function
+  const renderTweets = function(tweetData) {
+    for (let tweet of tweetData) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
   }
 
-  const createTweetElement = function(tweetObj) {
+  // createTweetElement function
+  //takes in a tweet object and returns a tweet <article> element containing the entire HTML structure of the tweet.
+  const createTweetElement = function(tweetData) {
 
+    const timeStamp = timeago.format(tweetData.created_at);
 
-    const timeStamp = timeago.format(tweetObj.created_at);
-
-    const newTweet =
+    let newTweet =
       $(`<article class="tweet">
-      <header>
-      <div>
-
-        <div class="userAvatar">
-          <img src=${tweetObj.user.avatars}>
-        </div>
-        <span class="userName">${tweetObj.user.name}</span>
-
-      </div>
-        <span class="userHandle">${tweetObj.user.handle}</span>
-      </header>
-
-      <div class="middle">
-        <p>${tweetObj.content.text}</p>
-      </div>
-
-      <footer>
-
-        <span>${timeStamp}</span>
-        <span class="tweetIcons">
-          <i class="fas fa-flag"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-heart"></i>
-        </span>
-
-      </footer>
-      </article>`);
+    <header>
+    <div>
+    
+    <div class="userAvatar">
+    <img src=${tweetData.user.avatars}>
+    </div>
+    <span class="userName">${tweetData.user.name}</span>
+    
+    </div>
+    <span class="userHandle">${tweetData.user.handle}</span>
+    </header>
+    
+    <div class="middle">
+    <p>${tweetData.content.text}</p>
+    </div>
+    
+    <footer>
+    
+    <span>${timeStamp}</span>
+    <span class="tweetIcons">
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
+    </span>
+    
+    </footer>
+    </article>`);
 
     return newTweet;
-
   }
 
+  renderTweets(tweetData);
 
-  const $tweet = createTweetElement(tweetData);
 
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+})
 
-});
+
